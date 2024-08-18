3d03b5b64f9292152656e97d7714f7ebb780bb00
Daniel Lacasse <daniel@nokee.dev>
2024-08-18 17:23:44 +0200

    Create prepare task to run everything to prepare sample development
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

--- samples-dev/plugins/external-build/src/main/java/dev/nokee/samples/externalbuilds/ExternalBuilds.java
+++ samples-dev/plugins/external-build/src/main/java/dev/nokee/samples/externalbuilds/ExternalBuilds.java
@@ -2,5 +2,8 @@ package dev.nokee.samples.externalbuilds;
 
 public interface ExternalBuilds {
     ExternalBuild getByName(String name);
+    default ExternalBuild named(String name) {
+        return getByName(name);
+    }
     ExternalBuild externalBuildAt(Object rootProject);
 }
