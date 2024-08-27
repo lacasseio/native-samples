bff6b9ae1aae351bef96a04b51c167639f439c91
Daniel Lacasse <daniel@nokee.dev>
2024-08-27 06:03:00 +0200

    Fix unavailable APIs on Gradle 7.2
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

--- cpp/library-with-tests/build-wrapper-plugin/src/main/java/org/gradle/samples/plugins/WrappedNativeLibraryPlugin.java
+++ cpp/library-with-tests/build-wrapper-plugin/src/main/java/org/gradle/samples/plugins/WrappedNativeLibraryPlugin.java
@@ -4,7 +4,7 @@ import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.artifacts.ModuleVersionIdentifier;
-import org.gradle.api.artifacts.type.ArtifactTypeDefinition;
+import org.gradle.api.attributes.Attribute;
 import org.gradle.api.attributes.AttributeContainer;
 import org.gradle.api.attributes.Usage;
 import org.gradle.api.component.PublishableComponent;
@@ -30,12 +30,12 @@ import javax.inject.Inject;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.gradle.api.artifacts.type.ArtifactTypeDefinition.ARTIFACT_TYPE_ATTRIBUTE;
 import static org.gradle.api.artifacts.type.ArtifactTypeDefinition.DIRECTORY_TYPE;
 import static org.gradle.api.artifacts.type.ArtifactTypeDefinition.ZIP_TYPE;
 import static org.gradle.language.cpp.CppBinary.LINKAGE_ATTRIBUTE;
 
 public class WrappedNativeLibraryPlugin implements Plugin<Project> {
+    private static final Attribute<String> ARTIFACT_TYPE_ATTRIBUTE = Attribute.of("artifactType", String.class);
     private final ImmutableAttributesFactory attributesFactory;
 
     @Inject
