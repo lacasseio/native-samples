3032d34e05d4467fab195c79b17755ce2c6a2da8
Daniel Lacasse <daniel@nokee.dev>
2024-08-28 17:47:45 +0200

    Fix Swift PM samples for C++
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

--- samples-dev/src/test/groovy/org/gradle/samples/ExecuteCppSamplesIntegrationTest.groovy
+++ samples-dev/src/test/groovy/org/gradle/samples/ExecuteCppSamplesIntegrationTest.groovy
@@ -77,7 +77,7 @@ class ExecuteCppSamplesIntegrationTest extends ExecuteSamplesIntegrationTest {
 
         SwiftPmRunner.create()
                 .withProjectDir(new File(sample.sampleDir, "list-library"))
-                .withArguments(withArgs("build"))
+                .withArguments("build")
                 .build()
 
         GradleRunner.create()
@@ -87,12 +87,12 @@ class ExecuteCppSamplesIntegrationTest extends ExecuteSamplesIntegrationTest {
 
         SwiftPmRunner.create()
                 .withProjectDir(new File(sample.sampleDir, "utilities-library"))
-                .withArguments(withArgs("build"))
+                .withArguments("build")
                 .build()
 
         SwiftPmRunner.create()
                 .withProjectDir(new File(sample.sampleDir, "app"))
-                .withArguments(withArgs("build"))
+                .withArguments("build")
                 .build()
     }
 
